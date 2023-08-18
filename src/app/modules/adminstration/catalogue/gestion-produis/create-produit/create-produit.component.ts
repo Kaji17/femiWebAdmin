14fa8ce094cde9f2b6bb13e249fca049e973bcdd
Katina Ouattara <ouattarapevirgile@gmail.com>
2023-08-18 10:14:00 +0000

    ajout component ajout produit

---
+++ src/app/modules/adminstration/catalogue/gestion-produis/create-produit/create-produit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-produit',
+  templateUrl: './create-produit.component.html',
+  styleUrls: ['./create-produit.component.scss']
+})
+export class CreateProduitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
