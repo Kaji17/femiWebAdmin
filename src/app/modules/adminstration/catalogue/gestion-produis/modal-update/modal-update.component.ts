10118a9ac8c69466467fa17c6a968f5a6ca537ab
Katina Ouattara <ouattarapevirgile@gmail.com>
2023-08-25 14:49:10 +0000

    ajout des modals

---
+++ src/app/modules/adminstration/catalogue/gestion-produis/modal-update/modal-update.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-update',
+  templateUrl: './modal-update.component.html',
+  styleUrls: ['./modal-update.component.scss']
+})
+export class ModalUpdateComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
