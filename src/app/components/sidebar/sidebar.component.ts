93f8fef3e7703be63899bf91a6838129e6686435
Katina Ouattara <ouattarapevirgile@gmail.com>
2023-08-29 15:35:08 +0000

    gestion des pages disponible

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavConstants } from 'src/app/constants/nav.const';
 import { navAdminItems } from 'src/app/constants/_nav';
+import { RolePermissionsService } from 'src/app/shared/services/role-permissions.service';
 
 export const NOTIF: any[] = [
   {  title: 'Dashboard', describe: 'describe',  icon: 'ni-tv-2 text-primary', date: '12/03/2003'},
@@ -28,10 +29,20 @@ export class SidebarComponent implements OnInit {
 
   isExpanded: boolean = true;
 
-  constructor(private router: Router) { }
+  public infoUser:any
+
+  constructor(private router: Router,
+    private rolePermission: RolePermissionsService) {
+    this.infoUser = JSON.parse(localStorage.getItem("user_info"));
+    this.rolePermission.setPermission(this.infoUser)
+   }
 
   ngOnInit() {
-   this.menuItems = NavConstants
+    // this.getPermission()
+    this.menuItems=this.rolePermission.getMenuPermission()
+    console.log("====Permission", this.menuItems)
+
+  //  this.menuItems = NavConstants
    this.notifs=[
     {  title: 'Dashboard', describe: 'describe',  icon: 'ni-tv-2 text-primary', date: '12/03/2003'},
     {  title: 'Notif1', describe: 'describe',  icon: 'ni-planet text-blue', date: '12/03/2003'},
@@ -50,4 +61,22 @@ export class SidebarComponent implements OnInit {
     localStorage.removeItem("user_info")
     localStorage.removeItem("user_email")
   }
+
+  // getPermission(){
+  //   if (this.infoUser.body.role.description) {
+  //     let encodedValue = this.infoUser.body.role.description;
+
+  //     // Décodez la valeur en base64
+  //     let decodedValue = atob(encodedValue);
+
+  //     // Parsez la valeur décodée pour obtenir un objet JSON
+  //     let decodedObject = JSON.parse(decodedValue);
+  //     this.menuItems = decodedObject;
+  //     console.log('===description décodé success',this.menuItems)
+  //   }
+  //   else{
+  //     console.log('===description vide')
+  //     this.menuItems = NavConstants;
+  //   }
+  // }
 }
