749bc150d017ffd5c59110fa7b8dd8d435be43af
Katina Ouattara <ouattarapevirgile@gmail.com>
2023-09-06 16:24:08 +0000

    image de photo de profil check

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -9,6 +9,8 @@ import { NavConstants } from "src/app/constants/nav.const";
 import { filter } from "rxjs/operators";
 import { BreadcrumbService } from "src/app/shared/services/breadcrumb.service";
 import { TranslateService } from "@ngx-translate/core";
+import { AngularFireMessaging } from "@angular/fire/compat/messaging";
+import { Configurable } from "src/app/core/config";
 
 @Component({
   selector: "app-navbar",
@@ -20,24 +22,35 @@ export class NavbarComponent implements OnInit {
   public listTitles: any[];
   public location: Location;
   public menuItems: any[];
-  public notifs: any[];
+  public notifs: any[] = [];
   public name = "Get Current Url Route Demo";
   public currentRoute: string;
   titlebreadcrumb: string;
   itemsbreadcrumb: any[];
   public profil: any;
   public adminName: string
-  public currentLang
+  public currentLang;
+  message_id: any =0
+
   constructor(
     location: Location,
     private element: ElementRef,
     private router: Router,
     private route: ActivatedRoute,
-    public translate : TranslateService
+    public translate : TranslateService,
+    private angularFireMessaging: AngularFireMessaging,
+    private configService: Configurable,
+
   ) {
     translate.addLangs(['en', 'fr']),
     translate.setDefaultLang('fr')
     this.location = location;
+    // window.addEventListener('storage', (event) => {
+    //   if (event.key === 'user_info') {
+    //     this.profil = event.newValue; // Mettez à jour l'image
+    //     this.cdr.detectChanges(); // Forcer la mise à jour du composant
+    //   }
+    // });
   }
 
   switchLang(lang:string){
@@ -48,42 +61,38 @@ export class NavbarComponent implements OnInit {
     this.currentLang='fr'
     this.profil = JSON.parse(localStorage.getItem("user_info"));
     this.menuItems = NavConstants;
-    this.notifs = [
-      {
-        title: "Dashboard",
-        describe: "describe",
-        icon: "ni-tv-2 text-primary",
-        date: "12/03/2003",
-      },
-      {
-        title: "Notif1",
-        describe: "describe",
-        icon: "ni-planet text-blue",
-        date: "12/03/2003",
-      },
-      {
-        title: "Notif2",
-        describe: "describe",
-        icon: "ni-pin-3 text-orange",
-        date: "12/03/2003",
-      },
-      {
-        title: "Notif3",
-        describe: "describe",
-        icon: "ni-single-02 text-yellow",
-        date: "12/03/2003",
-      },
-      {
-        title: "Notif14",
-        describe: "describe",
-        icon: "ni-key-25 text-info",
-        date: "12/03/2003",
-      },
-      
-      
-    ];
     this.currentRoute = this.route.snapshot.url.join("/");
     console.log("url", this.currentRoute);
+
+    this.angularFireMessaging.messages.subscribe((payload) => {
+      console.log('nouveau message ', payload);
+      this.notifs.push(payload.notification);
+      this.playNotificationSound();
+      let notifObj = {};
+      if (localStorage.getItem('nouveauMessage')) {
+        let a = JSON.parse(localStorage.getItem('nouveauMessage') as string);
+        notifObj = {
+          id: this.message_id,
+          title: payload?.notification?.title,
+          body: payload?.notification?.body,
+          data: payload?.data,
+          lue: false,
+        };
+        this.message_id = this.message_id+1
+        localStorage.setItem('messageId', JSON.stringify(this.message_id))
+        a.push(notifObj);
+        this.notifs = a
+        localStorage.setItem('nouveauMessage', JSON.stringify(a));
+      }
+
+      // this.messageService.add({
+      //   key: 'livreur',
+      //   sticky: false,
+      //   severity: 'warn',
+      //   summary: payload?.notification?.title,
+      //   detail: payload?.notification?.body,
+      // });
+    });
   }
   getTitle() {
     var titlee = this.location.prepareExternalUrl(this.location.path());
@@ -102,5 +111,28 @@ export class NavbarComponent implements OnInit {
   logOut() {
     localStorage.removeItem("user_info");
     localStorage.removeItem("user_email");
+  }
+
+  playNotificationSound() {
+    console.log('==AUDIO==');
+    const audio = new Audio();
+    audio.src = 'assets/sons/notifson.mp3'; // Remplacez par le chemin relatif vers votre fichier sonore
+    audio.load();
+    audio.play();
+  }
+
+  getImg(src: string) {
+    if (src) {
+      return src.replace(
+        this.configService.get("imgVar"),
+        this.configService.get("imgHttp")
+      ) as any;
+    }
+  }
+
+  getFisrtLetter(src: string) {
+    if (src) {
+      return src.substring(0, 1);
+    }
   }
 }
